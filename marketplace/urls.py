12e9fba4c0f5a3c32cdf11c7bd0950dc1ef1d8c0
swastik <swastikgorai@gmail.com>
2023-04-19 23:04:02 +0530

    test

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -21,7 +21,8 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('/', include('core.urls')),
     path('items/', include('item.urls')),
-    path('conversation/' , include('conversation.urls')),
+    path('conversation/', include('conversation.urls')),
 
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
