f8cba49cd37175462a87a2d8670f40dff84d1c95
swastik <swastikgorai@gmail.com>
2023-04-20 14:50:24 +0530

    git log

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -21,9 +21,7 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('/', include('core.urls')),
     path('items/', include('item.urls')),
     path('conversation/', include('conversation.urls')),
 
-]
-# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
