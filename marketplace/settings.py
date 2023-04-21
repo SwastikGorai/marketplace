5488573254d7412072d8d825b0581fb9e4800d57
swastik <swastikgorai@gmail.com>
2023-04-21 20:46:13 +0530

    fixed CSRF error

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -157,8 +157,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = None
 
 
-https://marketplace-production-c6d0.up.railway.app/
-
 CSRF_TRUSTED_ORIGINS = [
     'https://marketplace-production-c6d0.up.railway.app'
 ]
